b5a06cf1cb101e351ad8580e804c6c1d9761288c
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2024-12-21 16:47:35 +0300

    minor

--- resources/views/components/cards/collection.blade.php
+++ resources/views/components/cards/collection.blade.php
@@ -1,10 +1,12 @@
-<a wire:key="{{$collection['id']}}" class=" flex flex-col gap-2 max-w-80" href="{{route('collection-page', $collection['slug'])}}" wire:navigate>
+<a wire:key="{{$collection['id']}}" class=" flex flex-col gap-2 max-w-80 w-fit max-w-72 sm:mx-auto"
+   href="{{route('collection-page', $collection['slug'])}}" wire:navigate>
     <div class="w-72 h-72 md:w-52 md:h-52 relative">
-        <div class="bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 animate-pulse absolute w-full h-full z-0"></div>
-        <img src="{{$collection->getFIrstMediaUrl('cover')}}" class="w-full absolute w-full h-full z-10"  alt="">
+        <div
+            class="bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200 animate-pulse absolute w-full h-full z-0"></div>
+        <img src="{{$collection->getFIrstMediaUrl('cover')}}" class="w-full absolute w-full h-full z-10" alt="">
     </div>
-    <h2 class="text-3xl font-bold">{{$collection['name']}}</h2>
-    <div class="flex gap-2 text-xl">
+    <h2 class="text-3xl font-bold md:text-xl">{{$collection['name']}}</h2>
+    <div class="flex gap-2 text-xl md:text-base">
         <p>{{$collection->brand['name']}}</p>
         <x-icon name="flag-country-{{$collection->product[0]['country_code']}}" class="w-4"/>
         <p>{{$collection->product[0]->country['name']}}</p>
--- resources/views/components/cards/product.blade.php
+++ resources/views/components/cards/product.blade.php
@@ -1,6 +1,7 @@
-<a wire:key="{{$product['id']}}" href="{{route('product-page', $product['slug'])}}" class="swiper-slide flex flex-col gap-2 w-fit max-w-72 sm:mx-auto" wire:navigate>
+<a wire:key="{{$product['id']}}" href="{{route('product-page', $product['slug'])}}"
+   class="swiper-slide flex flex-col gap-2 w-fit max-w-72 sm:mx-auto" wire:navigate>
     <div class="w-72 h-72 md:w-52 md:h-52 bg-gray-50">
-        <img src="{{$product->getFIrstMediaUrl('cover')}}" class="w-full"  alt="">
+        <img src="{{$product->getFIrstMediaUrl('cover')}}" class="w-full" alt="">
     </div>
     <h2 class="text-3xl font-bold md:text-xl">{{$product['name']}}</h2>
     <div class="flex gap-2 text-xl md:text-base">
@@ -9,5 +10,8 @@
         <p>{{$product->country['name']}}</p>
     </div>
     <p class="text-xl">От <b>999 руб. за м2</b></p>
-    <p class="text-green-700 text-xl font-bold flex gap-2"><x-heroicon-s-check-circle class="w-4" />В наличии</p>
+    <p class="text-green-700 text-xl font-bold flex gap-2">
+        <x-heroicon-s-check-circle class="w-4"/>
+        В наличии
+    </p>
 </a>
